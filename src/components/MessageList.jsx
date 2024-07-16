d5db9b37213799a26ab0162a7e96bf7f164c685a
siddhesh1051 <siddhesh1052001@gmail.com>
2024-07-17 03:03:20 +0530

    FEATL Added a ref to scoll down

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import createSocket from "../socket";
 
 const MessageList = ({ currentGroup }) => {
   const [messages, setMessages] = useState([]);
   const [socket, setSocket] = useState(null);
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     const storedMessages =
@@ -39,26 +40,33 @@ const MessageList = ({ currentGroup }) => {
     };
   }, [currentGroup]);
 
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className="flex-1 overflow-y-auto p-1 w-full">
       <h1 className="mb-4 text-3xl font-bold text-center">
         Group {currentGroup}
       </h1>
       {messages.length !== 0 ? (
-        messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`my-2 p-2 rounded-lg sm:max-w-xs  max-w-40 break-words ${
-              msg.sender === "user"
-                ? "bg-blue-500 text-white ml-auto"
-                : "bg-gray-300 mr-auto"
-            }`}
-          >
-            {msg.content}
-          </div>
-        ))
+        <>
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`my-2 p-2 rounded-lg sm:max-w-xs max-w-40 break-words ${
+                msg.sender === "user"
+                  ? "bg-blue-500 text-white ml-auto"
+                  : "bg-gray-300 mr-auto"
+              }`}
+            >
+              {msg.content}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </>
       ) : (
-        <div className="text-center text-gray-500 mt-8 text-lg font-light ">
+        <div className="text-center text-gray-500 mt-8 text-lg font-light">
           No messages yet
         </div>
       )}
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
